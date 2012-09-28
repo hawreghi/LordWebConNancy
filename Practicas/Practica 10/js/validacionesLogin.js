0350801d493d80dd58e838c9cfcafdc84aa4b71e
hawreghi <hawreghi@hotmail.com>
2012-09-27 20:44:14 -0500

    practica diez! mi primera prácitca

---
+++ Practicas/Practica 10/js/validacionesLogin.js
@@ -0,0 +1,40 @@
+window.onload = function()
+{
+	document.getElementById("enviar").onclick = validar;	
+
+	function validar () 
+	{		
+		var form = document.login;
+		var regexEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+			
+		if(form.nick.value.length == 0)
+		{
+			alert("Nombre de usuario vacío");
+			form.nick.focus();
+		}
+		else if (!regexEmail.test(form.mail.value))
+		{
+			alert("Formato de correo no válido");
+			form.mail.focus();
+		}
+		else if (form.password.value.length == 0)
+		{
+			alert("No puedes dejar el campo de contraseña vacío");
+			form.password.focus();	
+		}
+		else if (form.password.value != form.confirmacion.value)
+		{
+			alert("Las contraseñas no coinciden");
+			form.confirmacion.focus();
+		}
+		else
+		{
+			var aCopiar = document.getElementById("password").value;
+			document.getElementById("password").value = "";
+			aCopiar = md5(aCopiar);
+			document.getElementById("md5").value = aCopiar;
+			
+			form.submit();
+		}	  
+	}
+}
